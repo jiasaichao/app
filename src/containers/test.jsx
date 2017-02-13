486682e57c8cbe0240502697baa6149688ea7f70
Opelar <23897631@qq.com>
2017-02-13 16:35:27 +0800

    拖动测试

--- src/containers/test.jsx
+++ src/containers/test.jsx
@@ -1,206 +1,109 @@
 import { NavBar, List, Container, Button, Icon, Animate, Gesture } from "../components/index";
 import React from 'react';
 import { Motion, spring } from 'react-motion';
+class Sort extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+
+        return (
+            <div>
+                {this.props.items.map((value, index) => {
+                    return React.cloneElement(this.props.children(value, index), {
+                        'data-sortid': index, key: index, onDropCapture: this.drop, onDragLeaveCapture: this.dragleave,
+                        onDragOverCapture: this.dragover, onDragEnterCapture: this.dragenter,
+                        onDragCapture: this.drag, onDragStartCapture: this.dragstart, draggable: 'true'
+                    });
+                })}
+            </div>
+        )
+    }
+    dragstart = (e) => {
+        //debugger;
+        //dataTransfer.setData()方法设置数据类型和拖动的数据
+        e.dataTransfer.setData("number", e.target.dataset.sortid);
+        console.log('拖动开始', e.target.innerHTML);
+    }
+    drag = (e) => {
+        //console.log('拖动到了本元素', e.target.innerHTML);
+    }
+    dragenter = (e) => {
+        e.target.style.fontSize = '28px'
+        console.log('拖入元素', e.target.innerHTML);
+    }
+    dragleave = (e) => {
+        e.target.style.fontSize = '16px'
+        console.log('离开拖动元素', e.target.innerHTML);
+    }
+    dragover = (e) => {
+        e.preventDefault();
+        //console.log('可被释放', e.target.innerHTML);
+    }
+    drop = (e) => {
+        e.preventDefault();
+        //移动到的id
+        let addid = parseInt(e.target.closest('[data-sortid]').dataset.sortid);
+        let delid = parseInt(e.dataTransfer.getData("number"));
+        if (addid == delid) {
+            return;
+        }
+        //debugger;
+        let list = [];
+        //往前移动则移动到前边元素的前边，往后移动则移动到后边元素的后边
+        this.props.items.forEach((value, index) => {
+            switch (index) {
+                case addid:
+                    if (delid > addid) {
+                        list.push(this.props.items[delid])
+                        list.push(this.props.items[index])
+
+                    } else {
+                        list.push(this.props.items[index])
+                        list.push(this.props.items[delid])
+                    }
+                    break
+                case delid:
+                    break
+                default:
+                    list.push(this.props.items[index])
+                    break
+            }
+
+        })
+        //let list=JSON.parse(JSON.stringify(this.state.list));
+        this.props.updateState(list);
+        //this.setState({list});
+    }
+    componentDidMount() {
+
+    }
+}
 class TestPage extends React.Component {
     constructor(props) {
         super(props)
-
-        this.width = 1920;
-        this.height = 1080;
-
-        this.startX = null;
-        this.startY = null;
-        this.clientX = null;
-        this.clientY = null;
-        this.targetX = null;
-        this.targetY = null;
         this.state = {
+            list: ['aaaaa1', 'aaaaa2', 'aaaaa3', 'aaaaa4', 'aaaaa5', 'aaaaa6', 'aaaaa7']
         }
-        //w<h宽小于高,以宽为基准
-        if (this.width < this.height) {
-            this.state.width = document.body.clientWidth;
-            this.state.height = this.height * (document.body.clientWidth / this.width);
-            this.state.top = document.body.clientHeight / 2 - this.state.height / 2
-            this.state.left = 0;
-        } else {
-            this.state.height = document.body.clientWidth;
-            this.state.width = this.width * (document.body.clientWidth / this.height);
-            this.state.top = (document.body.clientHeight - document.body.clientWidth) / 2;
-            this.state.left = -(this.state.width - document.body.clientWidth) / 2;
-        }
-    }
-    _minLeft = (nextWidth = this.state.width) => {
-        return -(nextWidth - document.body.clientWidth);
-    }
-    _maxLeft = () => {
-        return 0;
-    }
-    _minTop = (nextHeight = this.state.height) => {
-        return -(nextHeight - (document.body.clientHeight - document.body.clientWidth) / 2 - document.body.clientWidth);
-    }
-    _maxTop = () => {
-        return (document.body.clientHeight - document.body.clientWidth) / 2;
-    }
-    _offsetX = () => {
-        return this.clientX - this.startX + this.targetX;
-    }
-    _offsetY = () => {
-        return this.clientY - this.startY + this.targetY;
-    }
-    _touchStart = (e) => {
-        if (e.touches.length == 1) {
-            this.isMove = true;
-            this.startX = e.touches[0].clientX;
-            this.startY = e.touches[0].clientY;
-            this.targetX = this.state.left;
-            this.targetY = this.state.top;
-        }
-        else {
-            this.isMove = false;
-        }
-    }
-    _touchMove = (e) => {
-        if (this.props.preventDefault) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-        if (this.isMove) {
-            this.clientX = e.touches[0].clientX;
-            this.clientY = e.touches[0].clientY;
-            let setState = {};
-            if (this._offsetX() > this._minLeft() && this._offsetX() < this._maxLeft()) {
-                setState.left = this._offsetX()
-            }
-            if (this._offsetY() > this._minTop() && this._offsetY() < this._maxTop()) {
-                setState.top = this._offsetY()
-            }
-            this.setState(setState);
-        }
-
-    }
-    _touchEnd = (e) => {
-    }
-    _onPinch = (pinch) => {
-        let setState = {};
-        setState.width = this.width * pinch.scale;
-        setState.height = this.height * pinch.scale;
-
-        let addLeft = (setState.width - this.state.width) / 2;
-        setState.left = this.state.left - addLeft;
-        if (setState.left > this._maxLeft()) {
-            setState.left = this._maxLeft();
-        }
-        if (setState.left < this._minLeft(setState.width)) {
-            setState.left = this._minLeft(setState.width);
-        }
-
-        let addTop = (setState.height - this.state.height) / 2;
-        setState.top = this.state.left - addTop;
-        if (setState.top > this._maxTop()) {
-            setState.top = this._maxTop();
-        }
-        if (setState.top < this._minTop(setState.height)) {
-            setState.top = this._minTop(setState.height);
-        }
-        this.setState(setState);
-    }
-    ddd = () => {
-        let img = document.getElementById("img");
-        let crop_canvas;
-
-        crop_canvas = document.createElement('canvas');
-        let scale=this.width/this.state.width;
-        console.log(scale);
-        crop_canvas.width = document.body.clientWidth*scale;
-        crop_canvas.height = document.body.clientWidth*scale;
-        //console.log('left:', -this.state.left, 'top:', this.state.top-(document.body.clientHeight - document.body.clientWidth) / 2, '3', document.body.clientWidth, '5', this.state.width, '6', this.state.height);
-        console.log(img, -this.state.left*scale, (this.state.top-(document.body.clientHeight - document.body.clientWidth) / 2)*scale, document.body.clientWidth*scale, document.body.clientWidth*scale, 0, 0, document.body.clientWidth*scale, document.body.clientWidth*scale);
-        crop_canvas.getContext('2d').drawImage(img, -this.state.left*scale, (this.state.top-(document.body.clientHeight - document.body.clientWidth) / 2)*scale, document.body.clientWidth*scale, document.body.clientWidth*scale, 0, 0, document.body.clientWidth*scale, document.body.clientWidth*scale);
-     
-        window.open(crop_canvas.toDataURL("image/png"));
     }
     render() {
-        let pinch = {};
-        if (this.width < this.height) {
-            pinch.minScale = document.body.clientWidth / this.width;
-            pinch.maxScale = this.width / document.body.clientWidth;
-        } else {
-            pinch.minScale = document.body.clientWidth / this.height;
-            pinch.maxScale = this.height / document.body.clientWidth;
-        }
+
         return (
-            <div style={{ background: '#000', height: '100%', width: '100%', position: 'absolute',zIndex:9999 }}>
-                <div style={styles.mask} >
-                    <div style={styles.maskTop}></div>
-                    <div style={{ height: 1, background: '#fff', opacity: .5 }}></div>
-                    <Gesture.Touchable preventDefault={true} pinch={pinch} onPinch={this._onPinch} style={styles.maskCenter} onTouchStart={this._touchStart} onTouchMove={this._touchMove} onTouchEnd={this._touchEnd}></Gesture.Touchable>
-                    <div style={{ height: 1, background: '#fff', opacity: .5 }}></div>
-                    <div style={styles.maskTop}></div>
-                </div>
-                <div style={styles.imgContainer}>
-                    <img id='img' style={{ width: this.state.width, position: 'absolute', top: this.state.top, left: this.state.left, transform: `scale(${this.state.scale})` }} src='/image/portrait.jpg' />
-                </div>
-                <div style={{ position: 'absolute', display: 'flex', bottom: '0', zIndex: 1000, color: '#fff' }}>
-                    <Gesture.Touchable>取消</Gesture.Touchable>
-                    <Gesture.Touchable onTap={this.ddd}>完成1</Gesture.Touchable>
-                </div>
+            <div style={{ height: '100%', width: '100%', position: 'absolute', zIndex: 9999, display: 'flex' }}>
+                <Sort updateState={(list) => {
+                    this.setState({ list });
+                } } items={this.state.list} >{(value, index) => { return <div draggable='false'>{value}</div> } }</Sort>
             </div>)
     }
-
     componentDidMount() {
-        function drawBeauty(beauty) {
-            var canvas = document.getElementById('myCanvas');
-            canvas.width = document.body.clientWidth;
-            //canvas.height = 400;
-            var ctx = canvas.getContext('2d');
-            ctx.drawImage(beauty, 0, 0, 600, 400);
-        }
 
-        function load() {
-            var beauty = new Image();
-            beauty.src = "http://pic.4j4j.cn/upload/pic/20130815/31e652fe2d.jpg";
-            if (beauty.complete) {
-                drawBeauty(beauty);
-            } else {
-                beauty.onload = function () {
-                    drawBeauty(beauty);
-                };
-                beauty.onerror = function () {
-                    window.alert('美女加载失败，请重试');
-                };
-            }
-        }
     }
 }
 const styles = {
-    mask: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        flexDirection: 'column',
-        display: 'flex',
-        zIndex: '5'
-    },
-    maskTop: {
-        flex: '1',
-        background: '#000',
-        opacity: 0.4
-    },
-    maskCenter: {
-        height: '7.5rem',
-        background: '#000',
-        opacity: 0
-    },
-    imgContainer: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        display: 'flex',
-        alignItems: 'center'
+    item: {
+        height: 50,
+        fontSize: 16,
+        width: 80
     }
 }
 export default TestPage;
--- src/utils/public.js
+++ src/utils/public.js
@@ -20,3 +20,25 @@ if (typeof Object.assign != "function") {
         };
     })();
 }
+// matches polyfill
+this.Element && function(ElementPrototype) {
+    ElementPrototype.matches = ElementPrototype.matches ||
+    ElementPrototype.matchesSelector ||
+    ElementPrototype.webkitMatchesSelector ||
+    ElementPrototype.msMatchesSelector ||
+    function(selector) {
+        var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
+        while (nodes[++i] && nodes[i] != node);
+        return !!nodes[i];
+    }
+}(Element.prototype);
+
+// closest polyfill
+this.Element && function(ElementPrototype) {
+    ElementPrototype.closest = ElementPrototype.closest ||
+    function(selector) {
+        var el = this;
+        while (el.matches && !el.matches(selector)) el = el.parentNode;
+        return el.matches ? el : null;
+    }
+}(Element.prototype);
