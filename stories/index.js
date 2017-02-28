cc9f7e8623e5b5a127ec69939bbe2a0ea4fb8123
Opelar <23897631@qq.com>
2017-02-28 18:01:46 +0800

    storybooks

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,7 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
 import { List } from '../src/components';
+import { WithNotes } from '@kadira/storybook-addon-notes';
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
@@ -14,8 +15,24 @@ storiesOf('Button', module)
     <Button onClick={action('clicked')}>Hello Button</Button>
   ))
   .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ));
+    <WithNotes notes={'试试Here we use some emoji as the Button text. Isn\'t it look nice?'}>
+      <Button onClick={action('clicked')}>1111😀 😎 👍 💯</Button>
+    </WithNotes>
+  )).addWithInfo(
+    'simple usage',
+    `
+      This is the basic usage with the button with providing a label to show the text.
+    `,
+    () => (
+      <div>
+        <Button label="The Button" onClick={action('onClick')}/>
+        <br />
+        <p>
+          Click the "?" mark at top-right to view the info.
+        </p>
+      </div>
+    ),
+  );
 
 storiesOf('List', module).add('link', () => (
   <List.Link lable='列表3'></List.Link>
