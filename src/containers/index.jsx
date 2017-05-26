132ab02c2f0bfb6812e6c78e46d9ba0fb859d668
jiasaichao <jiasaichao@hotmail.com>
2017-05-26 17:15:04 +0800

    react-router 加入

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 export default class Index extends React.Component {
     render() {
         return (
-            <div>hello210 word1</div>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/">home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                    </ul>
+                    <div>hello210 word1</div>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                </div>
+            </Router>
         );
     }
 }
+const Home = ({ history }) => (
+    <div>
+        <h2 onClick={() => {
+            history.push('/about');
+        }}>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
